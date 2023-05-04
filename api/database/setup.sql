c4915d3658a7e239b5c8561795ee3bc65ea4ffc3
mithurssan <126930607+mithurssan@users.noreply.github.com>
2023-05-04 16:48:15 +0100

    update date data type to DATE

--- api/database/setup.sql
+++ api/database/setup.sql
@@ -2,12 +2,13 @@ DROP table IF EXISTS entries;
 
 CREATE TABLE entries (
     id INT GENERATED ALWAYS AS IDENTITY,
-    date VARCHAR(20) NOT NULL,
-    time time NOT NULL,
+    date DATE NOT NULL,
+    time TIME NOT NULL,
     category VARCHAR(20) NOT NULL,
     content VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 );   
 
+
 INSERT INTO entries (date, time, category, content)
-VALUES ('4th May 2023','00:00:00', 'Wildest Dream', 'Hello world.');
+VALUES ('2023/05/04','00:00:00', 'Wildest Dream', 'Hello world.');
