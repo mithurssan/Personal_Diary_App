b4aa033f01fa308e746191256c2339724392c182
kenenx <kenen1996@hotmail.co.uk>
2023-05-04 16:14:22 +0100

    add button links between pages

---
+++ client/assets/index.js
@@ -0,0 +1,5 @@
+function newEntryListener() {
+    const button = document.getElementById('new-entry-btn');
+    button.addEventListener('click', function () { window.location.href = '../create.html'
+    })
+}
